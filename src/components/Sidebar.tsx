7d3c53c31008e23548c623820c59a11531f68a74
PhucHuynh <huynhngocphuc.9d@gmail.com>
2025-04-01 17:48:32 +0700

    [Phuc] add sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,54 @@
-import React from "react";
-import { Drawer, Box } from "@mui/material";
+import React, { useState, Fragment } from "react";
+import { Drawer, Box, List, ListItemButton } from "@mui/material";
+// or
+import { ListSubheader } from "@mui/material";
+import { ListItem } from "@mui/material";
+import { ListItemIcon } from "@mui/material";
+import { ListItemText } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import Collapse from "@mui/material/Collapse";
+import ExpandLess from "@mui/icons-material/ExpandLess";
+import ExpandMore from "@mui/icons-material/ExpandMore";
+import { Label } from "@mui/icons-material";
+import HomeMaxOutlinedIcon from '@mui/icons-material/HomeMaxOutlined';
 
 const Sidebar = () => {
+  const [submenuState, setSubmenuState] = useState<{ [key: string]: boolean }>({});
+  const handleSubmenuToggle = (key: string) => {
+    setSubmenuState((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key], // Toggle the state for the specific submenu
+    }));
+  };
+  const menuItems = [
+    {
+      label: "dashboard",
+      icon: <HomeMaxOutlinedIcon />,
+      hasSubmenu: true,
+      key: "dashboard",
+      submenu: [
+        {
+          label: "Subitem 1",
+          icon: <HomeIcon />,
+        },
+        {
+          label: "Subitem 2",
+          icon: <HomeIcon />,
+        },
+      ],
+    },
+    { label: "Orders", icon: <HomeIcon />, hasSubmenu: false },
+    {
+      label: "Products",
+      icon: <HomeIcon />,
+      hasSubmenu: true,
+      key: "products",
+      submenu: [
+        { label: "Subitem A", icon: <HomeIcon /> },
+        { label: "Subitem B", icon: <HomeIcon /> },
+      ],
+    },
+  ];
   return (
     <Drawer variant="persistent" open={true}>
       <Box className="sidebarBox">
@@ -11,8 +58,35 @@ const Sidebar = () => {
             <h1 className="logo_text">GirlHaf</h1>
           </a>
         </Box>
-        <Box className="sidebarMenu">
-          
+        <Box className="sidebarMenu" sx={{ padding: "0 20px" }}>
+          <List>
+            <ListSubheader>Menu</ListSubheader>
+
+            {menuItems.map((item, index) => (
+              <Fragment key={index}>
+                <ListItemButton
+                  onClick={item.hasSubmenu ? () => handleSubmenuToggle(item.key || "") : undefined}
+                >
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText>{item.label}</ListItemText>
+                  {item.hasSubmenu &&
+                    (submenuState[item.key || ""] ? <ExpandLess /> : <ExpandMore />)}
+                </ListItemButton>
+                {item.hasSubmenu && (
+                  <Collapse in={submenuState[item.key || ""]} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      {item.submenu?.map((subitem, subIndex) => (
+                        <ListItemButton key={subIndex} sx={{ pl: 4 }}>
+                          <ListItemIcon>{subitem.icon}</ListItemIcon>
+                          <ListItemText>{subitem.label}</ListItemText>
+                        </ListItemButton>
+                      ))}
+                    </List>
+                  </Collapse>
+                )}
+              </Fragment>
+            ))}
+          </List>
         </Box>
       </Box>
     </Drawer>
